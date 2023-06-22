cc190c3a8fcc259b433be80b17e7b23eefd0d9e7
Kasireddi Sai Kumar <saikumar.kasireddi@mutualmobile.com>
2023-06-22 14:03:12 +0530

    Initial Commit

---
+++ BugFixAppCenter/BugFixAppCenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugFixAppCenterApp.swift
+//  BugFixAppCenter
+//
+//  Created by Sai Kumar Kasireddi on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct BugFixAppCenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
